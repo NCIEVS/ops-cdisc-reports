d2007919a4aea296cf36b05c6b96a653aad5f96e
akuppusamy-wci <akuppusamy@westcoastinformatics.com>
2022-05-05 16:15:17 -0700

    NM-142: Create Lambda for OWL Report

---
+++ owl-report/src/main/java/gov/nih/nci/evs/cdisc/report/aws/LambdaHandler.java
@@ -0,0 +1,43 @@
+package gov.nih.nci.evs.cdisc.report.aws;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import gov.nih.nci.evs.cdisc.report.RDFGenerator;
+import gov.nih.nci.evs.cdisc.report.ReportEnum;
+import gov.nih.nci.evs.cdisc.report.model.ReportDetail;
+import gov.nih.nci.evs.cdisc.report.model.ReportSummary;
+import gov.nih.nci.evs.cdisc.report.utils.AssertUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
+public class LambdaHandler implements RequestHandler<ReportSummary, ReportSummary> {
+  Logger log = LoggerFactory.getLogger(LambdaHandler.class);
+
+  @Override
+  public ReportSummary handleRequest(ReportSummary input, Context context) {
+    validate(input);
+    for (ReportDetail reportDetail : input.getReportDetails()) {
+      String excelFile = reportDetail.getReports().get(ReportEnum.MAIN_TEXT);
+      if (excelFile != null && new File(excelFile).exists()) {
+        String baseName = FilenameUtils.removeExtension(excelFile);
+        String owlFile = baseName + ".owl";
+
+        RDFGenerator rdfGenerator = new RDFGenerator();
+        rdfGenerator.generate(excelFile, owlFile);
+        reportDetail.getReports().put(ReportEnum.MAIN_OWL, owlFile);
+      } else {
+        throw new RuntimeException(String.format("File %s does not exist.", excelFile));
+      }
+    }
+    // The Excel file report gets formatted in place, so
+    return input;
+  }
+
+  private void validate(ReportSummary request) {
+    AssertUtils.assertRequired(request.getReportDetails(), "reportDetails");
+    AssertUtils.assertRequired(request.getPublicationDate(), "publicationDate");
+  }
+}
