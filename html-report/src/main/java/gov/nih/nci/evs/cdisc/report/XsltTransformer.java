4fa380b081cd2f36a41c6475d8b90ec22504a90c
akuppusamy-wci <akuppusamy@westcoastinformatics.com>
2022-05-09 16:52:04 -0700

    NM-143: Create Lambda for HTML Report
    Fixed gobbling of exception in XsltTransformer

--- html-report/src/main/java/gov/nih/nci/evs/cdisc/report/XsltTransformer.java
+++ html-report/src/main/java/gov/nih/nci/evs/cdisc/report/XsltTransformer.java
@@ -14,18 +14,17 @@ public class XsltTransformer {
     StringWriter sw = new StringWriter();
     Source xslt = new StreamSource(XsltTransformer.class.getResource(xsltFilePath).getPath());
     Source xml = new StreamSource(xmlFilePath);
-    try (FileWriter fw = new FileWriter(htmlFilePath)){
+    try (FileWriter fw = new FileWriter(htmlFilePath)) {
       TransformerFactory tFactory = TransformerFactory.newInstance();
       Transformer transform = tFactory.newTransformer(xslt);
       transform.transform(xml, new StreamResult(sw));
       fw.write(sw.toString());
     } catch (Exception e) {
-      e.printStackTrace();
+      throw new RuntimeException(
+          String.format(
+              "Error occurred when converting XML to HTML. xml:%s xslt:%s",
+              xmlFilePath, xsltFilePath),
+          e);
     }
-  }
-
-  public static void main(String[] args) {
-    //XsltTransformer.convertXmlToHtml("/Users/squareroot/wci/ncievs-CDISC/Current/ADaM/ADaM Terminology.odm.xml", "/Users/squareroot/temp/ADaM Terminology.html", "/xslt/controlledterminology1-0-0.xsl");
-    XsltTransformer.convertXmlToHtml("/Users/squareroot/wci/ncievs-CDISC/Current/ADaM/ADaM Terminology.odm.xml", "/Users/squareroot/temp/ADaM Terminology-pdf.html", "/xslt/controlledterminology1-0-0-pdf.xsl");
   }
 }
